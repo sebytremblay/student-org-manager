30ad13aebc47215e22fd5ced5eb9f2501f4802ba
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-04-01 18:06:19 -0400

    Inserting sample data

--- phase02/studentOrg_roles_emails_socials.sql
+++ phase02/studentOrg_roles_emails_socials.sql
@@ -1,3 +1,9 @@
+-- ==================================
+--
+-- TABLE DEFINITIONS
+--
+-- ==================================
+
 -- Stores information about a student organization
 CREATE TABLE IF NOT EXISTS StudentOrg
 (
@@ -15,7 +21,7 @@ CREATE TABLE IF NOT EXISTS Roles
     positionName VARCHAR(30),
     year INT,
     semester VARCHAR(10),
-    PRIMARY KEY(userID, orgID),
+    PRIMARY KEY(userID, orgID, positionName, year, semester),
     FOREIGN KEY(userID)
         REFERENCES Users(ID)
         ON UPDATE CASCADE
@@ -50,3 +56,32 @@ CREATE TABLE IF NOT EXISTS SocialsInfo
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
+
+-- ==================================
+--
+-- SAMPLE DATA
+--
+-- ==================================
+INSERT INTO StudentOrg (orgID, name, establishedAt, orgType) VALUES
+(1, 'Soccer', '2010-09-01 00:00:00', 'Club Sports'),
+(2, 'Roller Hockey', '2012-05-15 00:00:00', 'Club Sports'),
+(3, 'Delta Tau Delta', '2011-08-20 00:00:00', 'Greek Life'),
+(4, 'Delta Zeta', '2013-03-05 00:00:00', 'Greek Life');
+
+INSERT INTO Roles (userID, orgID, positionName, year, semester) VALUES
+(1, 1, 'President', 2022, 'Fall'),
+(2, 2, 'Vice President', 2023, 'Spring'),
+(3, 3, 'Treasurer', 2022, 'Fall'),
+(4, 4, 'Secretary', 2023, 'Spring');
+
+INSERT INTO EmailInfo (userID, email) VALUES
+(1, 'user1@example.com'),
+(2, 'user2@example.com'),
+(3, 'user3@example.com'),
+(4, 'user4@example.com');
+
+INSERT INTO SocialsInfo (userID, platform, username) VALUES
+(1, 'Twitter', 'user1_tw'),
+(2, 'Instagram', 'user2_ig'),
+(3, 'Facebook', 'user3_fb'),
+(4, 'LinkedIn', 'user4_ln');
