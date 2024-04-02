ab27fab2fd100eac448fa5e00e6f55ca0df04c8e
drewskotarczaknu <94199600+drewskotarczaknu@users.noreply.github.com>
2024-04-02 13:03:17 -0400

    Update club-sport-events.sql

--- phase02/club-sport-events.sql
+++ phase02/club-sport-events.sql
@@ -1,3 +1,10 @@
+-- ==================================
+--
+-- TABLE DEFINITIONS
+--
+-- ==================================
+
+-- Stores information about drills to be used in practice events
 CREATE TABLE Drill (
     drillID int NOT NULL AUTO_INCREMENT,
     name varchar(100),
@@ -5,6 +12,7 @@ CREATE TABLE Drill (
     PRIMARY KEY(drillID)
 );
 
+-- Stores information about each practice event
 CREATE TABLE Practice (
     eventID int,
     practiceID int NOT NULL AUTO_INCREMENT,
@@ -15,6 +23,8 @@ CREATE TABLE Practice (
         REFERENCES Event (eventID)
 );
 
+-- Bridge table between practices and drills storing information about which drills were used in which practices
+-- Used to create practice plan for each practice
 CREATE TABLE DrillInstance (
     practiceID int NOT NULL,
     drillID int NOT NULL,
@@ -25,23 +35,29 @@ CREATE TABLE DrillInstance (
         REFERENCES Drill (drillID)
 );
 
-INSERT INTO Drill (drillID, name, description)
-VALUES  (1, "Butterfly", "half of team lines up in each corner one by one" + "skating around the offensive zone circle and" + "recieveing a pass then taking a shot"),
-        (2, "Breakout", "one player behind net, two wings swing low while last player" + "swings way high behind the other teams net"),
-        (3, "PowerPlay/PenaltyKill", "four offensive players, three defensive players." + " work a diamond and rotate into a box for PP," + "work a triangle for PK"),
-        (4, "Scrimmage", "four versus four");
+-- ==================================
+--
+-- SAMPLE DATA
+--
+-- ==================================
 
-INSERT INTO practice (eventID, practiceID, team, injuriesDesc)
-VALUES  (?, 1, "All", "none"),
-        (?, 2, "D2", "none"),
-        (?, 3, "D4", "puck to chest, knocked wind out of player");
+INSERT INTO Drill (drillID, name, description) VALUES  
+(1, "Butterfly", "half of team lines up in each corner one by one" + "skating around the offensive zone circle and" + "recieveing a pass then taking a shot"),
+(2, "Breakout", "one player behind net, two wings swing low while last player" + "swings way high behind the other teams net"),
+(3, "PowerPlay/PenaltyKill", "four offensive players, three defensive players." + " work a diamond and rotate into a box for PP," + "work a triangle for PK"),
+(4, "Scrimmage", "four versus four");
 
-INSERT INTO DrillInstance (practiceID, drillID)
-VALUES  (1, 1),
-        (1, 2),
-        (1, 3),
-        (1, 4),
-        (2, 1),
-        (2, 3),
-        (2, 4),
-        (3, 4);
+INSERT INTO practice (eventID, practiceID, team, injuriesDesc) VALUES
+(?, 1, "All", "none"),
+(?, 2, "D2", "none"),
+(?, 3, "D4", "puck to chest, knocked wind out of player");
+
+INSERT INTO DrillInstance (practiceID, drillID) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(2, 1),
+(2, 3),
+(2, 4),
+(3, 4);
