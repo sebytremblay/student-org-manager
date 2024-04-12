c8e85fcb3856e2270cd2e9cc50584f5001fb2cc7
drewskotarczaknu <94199600+drewskotarczaknu@users.noreply.github.com>
2024-04-12 14:36:20 -0400

    Add files via upload
    User Blueprint with the following routes:
    - Create User
    - Get User
    - Update User
    - Delete User
    - Get Orgs user belongs to
    - Get outstanding dues for user
    - Get schedule based on what organizations they belong to
    - Add user to student organization (this one needs to be edited to add a name for the role they are, going to get to it after I'm done with mock data creation)

---
+++ users.py
@@ -0,0 +1,120 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+users = Blueprint('users', __name__)
+
+# Create User
+@users.route('/users', methods=['POST'])
+def create_user():
+    user_data = request.json
+    cursor = db.get_db().cursor()
+    query = 'INSERT INTO Users (NUID, graduationYear, startDate, currentYear, firstName, lastName, goalsWants, challenges) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+    data = (
+        user_data['NUID'],
+        user_data['graduationYear'],
+        user_data['startDate'],
+        user_data['currentYear'],
+        user_data['firstName'],
+        user_data['lastName'],
+        user_data['goalsWants'],
+        user_data['challenges']
+    )
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'User created successfully'
+
+# Get User
+@users.route('/users/<userID>', methods=['GET'])
+def get_user(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM Users WHERE userID = %s', (userID,))
+    user_data = cursor.fetchone()
+    if not user_data:
+        return 'User not found', 404
+    user_dict = {
+        'userID': user_data[0],
+        'NUID': user_data[1],
+        'graduationYear': user_data[2],
+        'startDate': user_data[3],
+        'currentYear': user_data[4],
+        'firstName': user_data[5],
+        'lastName': user_data[6],
+        'goalsWants': user_data[7],
+        'challenges': user_data[8]
+    }
+    return jsonify(user_dict)
+
+# Update User
+@users.route('/users/<userID>', methods=['PUT'])
+def update_user(userID):
+    user_data = request.json
+    cursor = db.get_db().cursor()
+    query = 'UPDATE Users SET NUID = %s, graduationYear = %s, startDate = %s, currentYear = %s, firstName = %s, lastName = %s, goalsWants = %s, challenges = %s WHERE userID = %s'
+    data = (
+        user_data['NUID'],
+        user_data['graduationYear'],
+        user_data['startDate'],
+        user_data['currentYear'],
+        user_data['firstName'],
+        user_data['lastName'],
+        user_data['goalsWants'],
+        user_data['challenges'],
+        userID
+    )
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'User updated successfully'
+
+# Delete User
+@users.route('/users/<userID>', methods=['DELETE'])
+def delete_user(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('DELETE FROM Users WHERE userID = %s', (userID,))
+    db.get_db().commit()
+    return 'User deleted successfully'
+
+# Get Orgs user belongs to
+@users.route('/users/<userID>/orgs', methods=['GET'])
+def get_user_orgs(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT orgID, name FROM StudentOrgs WHERE orgID IN (SELECT orgID FROM Roles WHERE userID = %s)', (userID,))
+    orgs_data = cursor.fetchall()
+    orgs_list = [{'orgID': org[0], 'name': org[1]} for org in orgs_data]
+    return jsonify(orgs_list)
+
+# Get outstanding dues for user
+@users.route('/users/<userID>/dues', methods=['GET'])
+def get_user_dues(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT amount, dueDate FROM Dues WHERE userID = %s', (userID,))
+    dues_data = cursor.fetchall()
+    dues_list = [{'amount': due[0], 'dueDate': due[1]} for due in dues_data]
+    return jsonify(dues_list)
+
+# Get schedule based on what organizations they belong to
+@users.route('/users/<userID>/schedule', methods=['GET'])
+def get_user_schedule(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM Events WHERE orgID IN (SELECT orgID FROM Roles WHERE userID = %s)', (userID,))
+    events_data = cursor.fetchall()
+    events_list = [{
+        'eventID': event[0],
+        'orgID': event[1],
+        'name': event[2],
+        'startTime': event[3],
+        'endTime': event[4],
+        'location': event[5],
+        'isMandatory': event[6]
+    } for event in events_data]
+    return jsonify(events_list)
+
+# Add user to student organization
+@users.route('/users/<userID>/orgs/<orgID>', methods=['POST'])
+def add_user_to_org(userID, orgID):
+    cursor = db.get_db().cursor()
+    query = 'INSERT INTO Roles (userID, orgID) VALUES (%s, %s)'
+    data = (userID, orgID)
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'User added to organization successfully'
